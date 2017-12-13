ffe361f84fb624fd6a19f1b7f4aa4d789e631b2e
Pavel Kiselev <pavel.kiselyov@protonmail.com>
2017-12-13 17:21:08 +0700

    Redis communication

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 #![feature(plugin)]
 #![plugin(rocket_codegen)]
 
+extern crate r2d2;
+extern crate r2d2_redis;
+extern crate redis;
 extern crate rocket;
 #[macro_use]
 extern crate rocket_contrib;
@@ -9,6 +12,8 @@ extern crate serde_derive;
 
 mod system;
 mod message;
+mod db;
+mod items;
 
 use std::collections::HashMap;
 use std::sync::Mutex;
@@ -16,11 +21,13 @@ use std::sync::Mutex;
 fn rocket() -> rocket::Rocket {
     rocket::ignite()
         .mount("/", routes![system::healthcheck])
+        .mount("/items", routes![items::create, items::index])
         .mount(
             "/message",
             routes![message::new, message::update, message::get],
         )
         .catch(errors![system::not_found, system::bad_request])
+        .manage(db::pool())
         .manage(Mutex::new(HashMap::<message::ID, String>::new()))
 }
 
