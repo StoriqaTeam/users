d0d5c373941754d7dcd4bdc4a6b5babb069b37d2
Alexey Karasev <karasev.alexey@gmail.com>
2018-01-17 17:59:55 +0300

    interim commit

--- src/macros/validation_errors.rs
+++ src/macros/validation_errors.rs
@@ -1,5 +1,40 @@
 macro_rules! validation_errors {
-    ($k: tt -> $v: tt) => {
-        // let errors = validation::
+    ($($field:tt => ($($code:tt -> $value:expr),+)),*) => {{
+        use validator;
+        use std::borrow::Cow;
+        use std::collections::HashMap;
+
+        let mut errors = validator::ValidationErrors::new();
+        $(
+            $(
+                let error = validator::ValidationError {
+                    code: Cow::from($code),
+                    message: Some(Cow::from($value)),
+                    params: HashMap::new(),
+                };
+
+                errors.add($field, error);
+            )+
+        )*
+
+        errors
+    }}
+}
+
+
+#[cfg(test)]
+mod tests {
+    use std::vec::Vec;
+
+    #[test]
+    fn several_errors() {
+        let errors = validation_errors!(
+            "email" => ("invalid" -> "Invalid email", "exists" -> "Already exists"),
+            "password" => ("match" -> "Doesn't match")
+        );
+        let errors = errors.inner();
+        let error = errors.get("email").unwrap().nth(0);
+        assert_eq!(error.code.into_owned(), "invalid");
+        // assert_eq!(&errors.get("email").unwrap()[0].message.unwrap().into_owned(), "invalid");
     }
 }
--- src/payloads/user.rs
+++ src/payloads/user.rs
@@ -5,7 +5,7 @@ use validator::Validate;
 #[derive(Serialize, Deserialize, Insertable, Validate, Clone)]
 #[table_name = "users"]
 pub struct NewUser {
-    #[validate(email(message = "Invalid e-mail format"))]
+    #[validate(email(message = "Invalid email format"))]
     pub email: String,
     #[validate(length(min = "6", max = "30", message = "Password should be between 6 and 30 symbols"))]
     pub password: String,
