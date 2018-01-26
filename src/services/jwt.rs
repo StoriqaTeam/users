4c5e537b3241f610ff335e3b66221dc58146465f
serejkaaa512 <5125402@mail.ru>
2018-01-26 14:57:42 +0300

    removed code section

--- src/services/jwt.rs
+++ src/services/jwt.rs
@@ -2,8 +2,7 @@ use futures::future;
 use futures::{Future, IntoFuture};
 use futures_cpupool::CpuPool;
 use hyper::{Method, Headers};
-use hyper::header::{Authorization, Bearer, ContentLength, ContentType};
-use hyper::mime::{APPLICATION_WWW_FORM_URLENCODED};
+use hyper::header::{Authorization, Bearer};
 use jsonwebtoken::{encode, Header};
 
 
@@ -143,36 +142,17 @@ impl<U: UsersRepo> JWTService for JWTServiceImpl<U> {
     ) -> ServiceFuture<JWT> {
 
         let jwt_secret_key = self.jwt_config.secret_key.clone();
-        let code_to_token_url = self.google_config.code_to_token_url.clone();
-        let redirect_url = self.google_config.redirect_url.clone();
-        let client_id = self.google_config.id.clone();
-        let client_secret = self.google_config.key.clone();
         let info_url = self.google_config.info_url.clone();
         let http_client = self.http_client.clone();
 
-        let exchange_code_to_token_url = format!("{}", code_to_token_url );
-        let body = format!("code={}&redirect_uri={}&client_id={}&client_secret={}&scope=&grant_type=authorization_code",
-            oauth.code,
-            redirect_url,
-            client_id,
-            client_secret
-            );
-        
-        let mut headers =  Headers::new();
-        headers.set(ContentLength(body.len() as u64 ) );
-        headers.set(ContentType(APPLICATION_WWW_FORM_URLENCODED));
+        let mut headers = Headers::new();
+        headers.set( Authorization ( Bearer {
+                    token: oauth.token
+        }));
 
         Box::new(
-            http_client.request::<GoogleToken>(Method::Post, exchange_code_to_token_url, Some(body), Some(headers))
-                .map_err(|e| Error::HttpClient(format!("Failed to connect to google oauth. {}", e.to_string())))
-                .and_then(move |token| {
-                    let mut headers = Headers::new();
-                    headers.set( Authorization ( Bearer {
-                                token: token.access_token
-                            }));
-                    http_client.request::<GoogleID>(Method::Get, info_url, None, Some(headers))
-                        .map_err(|e| Error::HttpClient(format!("Failed to receive user info from google. {}", e.to_string())))
-                })
+                http_client.request::<GoogleID>(Method::Get, info_url, None, Some(headers))
+                .map_err(|e| Error::HttpClient(format!("Failed to receive user info from google. {}", e.to_string())))
                 .and_then(move |google_id| {
                     let tokenpayload = JWTPayload::new(google_id.email);
                     encode(&Header::default(), &tokenpayload, jwt_secret_key.as_ref())
@@ -191,28 +171,14 @@ impl<U: UsersRepo> JWTService for JWTServiceImpl<U> {
     ) -> ServiceFuture<JWT> {
 
         let jwt_secret_key = self.jwt_config.secret_key.clone();
-        let code_to_token_url = self.facebook_config.code_to_token_url.clone();
-        let redirect_url = self.facebook_config.redirect_url.clone();
-        let client_id = self.facebook_config.id.clone();
-        let client_secret = self.facebook_config.key.clone();
         let info_url = self.facebook_config.info_url.clone();
         let http_client = self.http_client.clone();
 
-        let exchange_code_to_token_url = format!("{}?client_id={}&redirect_uri={}&client_secret={}&code={}",
-            code_to_token_url,
-            client_id,
-            redirect_url,
-            client_secret,
-            oauth.code);
+        let url = format!("{}?fields=first_name,last_name,gender,email,name&access_token={}", info_url, oauth.token);
 
         let future =
-            http_client.request::<FacebookToken>(Method::Get, exchange_code_to_token_url, None, None)
-                .map_err(|e| Error::HttpClient(format!("Failed to connect to facebook oauth. {}", e.to_string())))
-                .and_then(move |token| {
-                    let url = format!("{}?fields=first_name,last_name,gender,email,name&access_token={}", info_url, token.access_token);
-                    http_client.request::<FacebookID>(Method::Get, url, None, None)
-                        .map_err(|e| Error::HttpClient(format!("Failed to receive user info from facebook. {}", e.to_string())))
-                })
+                http_client.request::<FacebookID>(Method::Get, url, None, None)
+                .map_err(|e| Error::HttpClient(format!("Failed to receive user info from facebook. {}", e.to_string())))
                 .and_then(move |facebook_id| {
                     let tokenpayload = JWTPayload::new(facebook_id.email);
                     encode(&Header::default(), &tokenpayload, jwt_secret_key.as_ref())
