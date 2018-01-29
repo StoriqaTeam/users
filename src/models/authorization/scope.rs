10148d3dfa2394c42766b19507fc57f889f17890
Alexey Karasev <karasev.alexey@gmail.com>
2018-01-29 18:57:21 +0300

    added some more comments

--- src/models/authorization/scope.rs
+++ src/models/authorization/scope.rs
@@ -5,6 +5,8 @@ pub enum Scope {
     /// Resource with any id
     All,
 
-    /// Resource with id of the owner equal to the id of the current user
+    /// Resource with id of the owner equal to the id of the current user.
+    /// Important semantics - if you're using `Index` action with `Owned`, that
+    /// means that a user can only list resources that he owns.
     Owned,
 }
