1934c3b0dc7498a8a0c7ce67b2e2a1a5d9db3cf1
Maxim Vasilev <33905371+qwertys-ru@users.noreply.github.com>
2018-07-10 18:52:07 +0300

    logging

--- src/config.rs
+++ src/config.rs
@@ -1,7 +1,9 @@
 //! Config module contains the top-level config for the app.
+use std::env;
+
+use stq_logging::GrayLogConfig;
 
 use config_crate::{Config as RawConfig, ConfigError, Environment, File};
-use std::env;
 
 /// Basic settings - HTTP binding address and database DSN
 #[derive(Debug, Deserialize, Clone)]
@@ -12,6 +14,7 @@ pub struct Config {
     pub jwt: JWT,
     pub google: OAuth,
     pub facebook: OAuth,
+    pub graylog: Option<GrayLogConfig>,
 }
 
 /// Common server settings
--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,13 @@
 //! This create is for running the service from `users_lib`. See `users_lib` for details.
 
 extern crate users_lib;
+extern crate stq_logging;
 
 fn main() {
     let config = users_lib::config::Config::new().expect("Can't load app config!");
+
+    // Prepare logger
+    stq_logging::init(config.graylog.as_ref());
+
     users_lib::start_server(config);
 }
