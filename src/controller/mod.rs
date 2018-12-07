9652026f6b180d54764d8e475b16591dbba3b399
busarovalex <busarovalex@yandex.ru>
2018-12-07 11:10:47 +0300

    Revert "Clear database for testing purpose"
    This reverts commit 248926539cf257b853a7c4448a5194428c02977a.

--- src/controller/mod.rs
+++ src/controller/mod.rs
@@ -36,7 +36,6 @@ use sentry_integration::log_and_capture_error;
 use services::jwt::JWTService;
 use services::user_roles::UserRolesService;
 use services::users::UsersService;
-use services::util::UtilService;
 use services::Service;
 
 /// Controller handles route parsing and calling `Service` layer
@@ -97,9 +96,6 @@ impl<
         let path = req.path().to_string();
 
         let fut = match (&req.method().clone(), self.static_context.route_parser.test(req.path())) {
-            // POST /clear_database
-            (&Post, Some(Route::ClearDatabase)) => serialize_future(service.clear_database()),
-
             // GET /users/<user_id>
             (&Get, Some(Route::User(user_id))) => serialize_future(service.get(user_id)),
 
--- src/services/util.rs
+++ src/services/util.rs
@@ -1,45 +1,10 @@
 use base64::{decode, encode};
-use diesel::connection::AnsiTransactionManager;
-use diesel::pg::Pg;
-use diesel::query_dsl::RunQueryDsl;
-use diesel::Connection;
-use r2d2::ManageConnection;
 use rand;
 use rand::Rng;
-use services::Service;
 use sha3::{Digest, Sha3_256};
 
-use super::types::ServiceFuture;
 use errors::Error;
-use repos::repo_factory::ReposFactory;
 use repos::types::RepoResult;
-
-pub trait UtilService {
-    fn clear_database(&self) -> ServiceFuture<()>;
-}
-
-impl<
-        T: Connection<Backend = Pg, TransactionManager = AnsiTransactionManager> + 'static,
-        M: ManageConnection<Connection = T>,
-        F: ReposFactory<T>,
-    > UtilService for Service<T, M, F>
-{
-    fn clear_database(&self) -> ServiceFuture<()> {
-        debug!("Truncating tables");
-        self.spawn_on_pool(|conn| {
-            let _ = diesel::sql_query("TRUNCATE TABLE identities, reset_tokens, user_roles, users;")
-                .execute(&*conn)?;
-            let _ = diesel::sql_query("INSERT INTO users (id, email, last_login_at, saga_id) VALUES (1, 'admin@storiqa.com', now(), 'a4cb84cb-62a7-45c6-939e-7c57cc399d5a') ON CONFLICT (id) DO NOTHING;")
-                .execute(&*conn)?;
-            let _ = diesel::sql_query("INSERT INTO identities (user_id, email, provider, password, saga_id) SELECT id, email, '', 'ivcHmQPHBx9EUGql4Zv8EaXCkQcswPuL905JCp5ss5k=.js5QVSk6FG', 'a4cb84cb-62a7-45c6-939e-7c57cc399d5a' FROM users WHERE email = 'admin@storiqa.com' LIMIT 1;")
-                .execute(&*conn)?;
-            let _ = diesel::sql_query("INSERT INTO user_roles (user_id, name) SELECT id, 'superuser' FROM users WHERE email = 'admin@storiqa.com' LIMIT 1;")
-                .execute(&*conn)?;
-
-            Ok(())
-        })
-    }
-}
 
 pub fn password_create(clear_password: String) -> String {
     let salt = rand::thread_rng().gen_ascii_chars().take(10).collect::<String>();
